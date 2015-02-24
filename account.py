3cc2036ddf1c90bc4d38fe0827836d7ef4390303
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2015-02-23 21:39:15 -0300

    [FIX] Bad identation. Pep8 compatible. Add identation help for vim.

--- account.py
+++ account.py
@@ -1,11 +1,13 @@
-from openerp.osv import osv,fields
-from datetime import date
+# -*- coding: utf-8 -*-
+from openerp.osv import osv
+
 
 class account_invoice(osv.osv):
-	_name = "account.invoice"
-	_inherit = "account.invoice"
+    _name = "account.invoice"
+    _inherit = "account.invoice"
 
-	def _convert_ref(self, ref):
-        return (ref or '').replace('/','')
+    def _convert_ref(self, ref):
+        return (ref or '').replace('/', '')
 
 account_invoice()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
